a610561d4144559f323d5665ffc793b7ca9e1caf
Joop Ringelberg <joop.ringelberg@gmail.com>
2025-03-28 11:16:04 +0100

    We can now run the Vite server on https. This enables us to sign a dev installation up to the BrokerService.

--- packages/mycontexts/vite.config.ts
+++ packages/mycontexts/vite.config.ts
@@ -5,14 +5,22 @@ import commonjs from 'vite-plugin-commonjs'
 import { visualizer } from 'rollup-plugin-visualizer'
 import del from 'rollup-plugin-delete'
 import { resolve } from 'path';
+import fs from 'fs';
 import postcss from 'rollup-plugin-postcss';
 import json from '@rollup/plugin-json';import { default as thepackage } from './package.json'
+
+// Get path to monorepo root
+const monorepoRoot = resolve(__dirname, '../..');
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/www/",
   server: {
-    port: 5177, // Set a fixed port
+    port: 5177,
+    https: {
+      key: fs.readFileSync(resolve(monorepoRoot, 'certificates/key.pem')),
+      cert: fs.readFileSync(resolve(monorepoRoot, 'certificates/cert.pem')),
+    },
     fs: {
       allow: [
         '..', // Allow serving files from one level up to the project root
@@ -56,6 +64,7 @@ export default defineConfig({
   },
   define: {
     __MYCONTEXTS_VERSION__: JSON.stringify(thepackage.version),
-    __STARTPAGE__: JSON.stringify("pub:https://perspectives.domains/cw_j4qovsczpm/#bxjprzq9q6$External")
+    __STARTPAGE__: JSON.stringify("pub:https://perspectives.domains/cw_j4qovsczpm/#bxjprzq9q6$External"),
+    __MyContextsContainer__: JSON.stringify("root")
   }
 })
