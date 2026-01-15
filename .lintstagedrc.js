3c96c60876df90aac2faffaac48045338864cfcc
Joop Ringelberg <joop.ringelberg@gmail.com>
2026-01-15 17:26:18 +0100

    build(monorepo): tightened the scope of eslint on commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,9 @@
 export default {
-  // Lint TS and JS files
-  "**/*.{ts,tsx,js,jsx}": [
+  // Lint TS and JS source and test files under packages on commit
+  "packages/**/src/**/*.{ts,tsx,js,jsx}": [
     "eslint --fix --config ./eslint.config.js",
-  ]
+  ],
+  "packages/**/test/**/*.{ts,tsx,js,jsx}": [
+    "eslint --fix --config ./eslint.config.js",
+  ],
 };
